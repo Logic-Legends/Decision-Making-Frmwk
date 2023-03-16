e1c588d98a9ff5b08fedd3bcd0518b4caed85685
Ekrem <ekremteke.uk@gmail.com>
2023-03-16 23:05:12 +0000

    Start page was created and added to home page, After second page I will link to there

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import Start from "../components/start-page/Start";
 import "./Home.css";
 import logo from "./logo.svg";
 
@@ -35,6 +35,7 @@ export function Home() {
 				<h1 className="message" data-qa="message">
 					{message}
 				</h1>
+				<Start />
 				<Link to="/about/this/site">About</Link>
 			</div>
 		</main>
