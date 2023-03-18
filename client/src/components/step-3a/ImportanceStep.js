5454c9c26ef9a2dcfe9465b164de7db4a625b2c8
mhallaq <malhallaq@gmail.com>
2023-03-18 13:19:07 +0000

    Conflict resolution

--- client/src/components/step-3a/ImportanceStep.js
+++ client/src/components/step-3a/ImportanceStep.js
@@ -5,10 +5,9 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import tooltipIcon from "./images/tooltipicon.png";
 
+// test conflict
 
-
-
-const ImportanceStep=()=>{
+const ImportanceStep = () => {
 	const [isStarted, setIsStarted] = useState(false);
 	const [isTooltipVisible, setIsTooltipVisible] = useState(false);
 
@@ -21,58 +20,51 @@ const ImportanceStep=()=>{
 	const handleTooltipClick = () => {
 		setIsTooltipVisible(!isTooltipVisible);
 	};
-     return (
-				<div className="header">
-					<h1>
-						Importance{" "}
-						<button className="btn-tooltip" onClick={handleTooltipClick}>
-							<img src={tooltipIcon} alt="Tooltip" className="tooltip-icon" />
-						</button>
-						{isTooltipVisible && (
-							<div
-								className={`tooltip ${isTooltipVisible ? "show" : ""}`}
-							>
-								Importance = the significance and/or value of the decision at
-								hand
-							</div>
-						)}
-					</h1>
-					<section className="question-container">
-						<p className="question">
-							How important is the decision?
-							<button className="btn-tooltip" onClick={handleTooltipClick}>
-								<img src={tooltipIcon} alt="Tooltip" className="tooltip-icon" />
-							</button>
-							{isTooltipVisible && (
-								<div
-									className={`tooltip ${
-										isTooltipVisible ? "show" : ""
-									}`}
-								>
-									Loading....
-								</div>
-							)}
-						</p>
-						<form className="radio-btn-section">
-							<label>
-								<input type="radio" name="importance" value="low" checked />
-								Low
-							</label>
-							<label>
-								<input type="radio" name="importance" value="high" />
-								High
-							</label>
-						</form>
-					</section>
-					<section className="btn-container">
-						<button>
-							<Link to="/"></Link>Back
-						</button>
-						{isStarted}
-						<button onClick={handleStartClick}>Next</button>
-					</section>
-				</div>
-			);
+	return (
+		<div className="header">
+			<h1>
+				Importance{" "}
+				<button className="btn-tooltip" onClick={handleTooltipClick}>
+					<img src={tooltipIcon} alt="Tooltip" className="tooltip-icon" />
+				</button>
+				{isTooltipVisible && (
+					<div className={`tooltip ${isTooltipVisible ? "show" : ""}`}>
+						Importance = the significance and/or value of the decision at hand
+					</div>
+				)}
+			</h1>
+			<section className="question-container">
+				<p className="question">
+					How important is the decision?
+					<button className="btn-tooltip" onClick={handleTooltipClick}>
+						<img src={tooltipIcon} alt="Tooltip" className="tooltip-icon" />
+					</button>
+					{isTooltipVisible && (
+						<div className={`tooltip ${isTooltipVisible ? "show" : ""}`}>
+							Loading....
+						</div>
+					)}
+				</p>
+				<form className="radio-btn-section">
+					<label>
+						<input type="radio" name="importance" value="low" checked />
+						Low
+					</label>
+					<label>
+						<input type="radio" name="importance" value="high" />
+						High
+					</label>
+				</form>
+			</section>
+			<section className="btn-container">
+				<button>
+					<Link to="/"></Link>Back
+				</button>
+				{isStarted}
+				<button onClick={handleStartClick}>Next</button>
+			</section>
+		</div>
+	);
 };
 
 export default ImportanceStep;
