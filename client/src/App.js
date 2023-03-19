1ba292cb7c0f81c1f23da79cf12a79af3bd79f41
mhallaq <malhallaq@gmail.com>
2023-03-19 21:38:38 +0000

    Removing duplicated components on the Apps.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Start from "./components/start-page/Start";
 import DecisionMakers from "./components/step-2/DecisionMakers";
 import Step1 from "./components/step-1/Step-1";
-import Step1 from "./components/step-1/Step-1";
-import DecisionMakers from "./components/step-2/DecisionMakers";
 const App = () => (
 	<div>
 	<header>
