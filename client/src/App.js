45e7ac287c8f285831116d8345387e6541ffeda1
Ekrem <ekremteke.uk@gmail.com>
2023-03-19 21:12:29 +0000

    Route was added to app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Start from "./components/start-page/Start";
-
+import Step1 from "./components/step-1/Step-1";
 const App = () => (
 	<div>
 	<header>
 
 	</header>
+
 	<Routes>
-		<Route path="/" element={<Start />} />
+		<Route path="/" element={<Start />} exact />
+		<Route path="/goal" element={<Step1 />} />
 	</Routes>
 <footer>
 
