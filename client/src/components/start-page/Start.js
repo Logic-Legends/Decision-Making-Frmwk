0b712c93cae1d0feeb4039753857613065678b2f
Ekrem <ekremteke.uk@gmail.com>
2023-03-21 22:24:00 +0000

    Update Start.js
    pdf code's was commited. I fixed

--- client/src/components/start-page/Start.js
+++ client/src/components/start-page/Start.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import Pdf from "../pdf-generation/Pdf";
-// import { PDFDownloadLink } from "@react-pdf/renderer";
+import Pdf from "../pdf-generation/Pdf";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 const Start = () => {
   return (
     <div>
