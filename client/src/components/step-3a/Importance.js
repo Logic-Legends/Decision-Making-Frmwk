72da24dd929708a105fe15c11c1c52c9a4c87939
leilafaez <leila.faez@gmail.com>
2023-03-21 19:35:37 +0000

    Update importance step by adding tooltip design as the same as step1

--- client/src/components/step-3a/Importance.js
+++ client/src/components/step-3a/Importance.js
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import tooltipIcon from "./images/tooltipicon.png";
+import HandleFirstTooltipClick from "./HandleFirstTooltipClick";
+import HandleSecondTooltipClick from "./HandleSecondTooltipClick";
+
 
 const Importance = () => {
 	//state for navigation on page by next and back btn
 	const [isStarted, setIsStarted] = useState(false);
 
-	//state for showing topic tooltip
-	const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+	//state for showing first topic tooltip
+	const [modalShow, setModalShow] = useState(false);
 
-	//state for question tooltip
-	const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+	//state for showing second topic tooltip
+	const [secondModalShow, setsecondModalShow] = useState(false);
 
 	//state for selecting radio btn
 	const [selectedOption, setSelectedOption] = useState(null);
 
 	//state for error handling
 	const [isModalOpen, setIsModalOpen] = useState(false);
-
 
 	const navigate = useNavigate();
 
@@ -29,6 +31,7 @@ const Importance = () => {
 		navigate("/decision-makers");
 	};
 
+	//click next btn handler
 	const handleNextClick = () => {
 		if (selectedOption === null) {
 			setIsModalOpen(true);
@@ -38,35 +41,31 @@ const Importance = () => {
 		}
 	};
 
-	//first tooltip handler
-	const handleTooltipClick = () => {
-		setIsTooltipVisible(!isTooltipVisible);
-	};
-	//second tooltip handler
-	const handleSecondTooltipClick = () => {
-		setIsTooltipOpen(!isTooltipOpen);
-	};
 	//radio btn handler
 	const handleOptionChange = (event) => {
 		setSelectedOption(event.target.value);
 	};
+
 	return (
 		<div className="container">
+			{/* call first tooltip component */}
+			<HandleFirstTooltipClick
+				show={modalShow}
+				onHide={() => setModalShow(false)}
+			/>
+			{/* call second tooltip component */}
+			<HandleSecondTooltipClick
+				show={secondModalShow}
+				onHide={() => setsecondModalShow(false)}
+			/>
 			<h1>
 				Importance{" "}
 				<img
 					src={tooltipIcon}
 					alt="Tooltip"
 					className="question-mark-pages"
-					onClick={handleTooltipClick}
+					onClick={() => setModalShow(true)}
 				/>
-				{/* tooltip for topic */}
-				{isTooltipVisible && (
-					<div className={`tooltip ${isTooltipVisible ? "show" : ""}`}>
-						<strong>Importance</strong> = the significance and/or value of the
-						decision at hand.
-					</div>
-				)}
 			</h1>
 			<section className="border-decision-framework-pages">
 				<p>
@@ -75,29 +74,9 @@ const Importance = () => {
 						src={tooltipIcon}
 						alt="Tooltip"
 						className="question-mark-pages"
-						onClick={handleSecondTooltipClick}
+						onClick={() => setsecondModalShow(true)}
 					/>
-					{/* tooltip for question */}
-					{isTooltipOpen && (
-						<div className={`tooltip ${isTooltipOpen ? "show" : ""}`}>
-							<strong>Guiding questions:</strong>
-							<br />
-							Will it affect how we operate as an organization or as a team
-							within the organization? Will it affect how we interact with other
-							organizations?
-							<br />
-							<br /> <strong>Example of low importance:</strong> The decision
-							will not likely affect the broader organization or how we interact
-							with other organizations. It will probably affect a small project
-							or small team within the organization.
-							<br />
-							<br />
-							<strong>Example of high importance:</strong> The decision will
-							likely affect the broader organization or how we interact with
-							other organizations. It will probably affect a large team within
-							the organization or how the organization operates at a high level.
-						</div>
-					)}
+					
 				</p>
 				{/* Radio btn section */}
 				<form className="radio-btn-section">
