1eb85af2816a9d754cb3d123401fce39edd9a399
Luan Souza <107760950+luan-murilo-de-oliveira-e-souza@users.noreply.github.com>
2023-03-22 22:00:33 +0000

    update error message on type of information

--- client/src/components/step-6a/TypeOfInformation.js
+++ client/src/components/step-6a/TypeOfInformation.js
@@ -1,15 +1,13 @@
-import React,{ useState,useRef } from "react";
+import React,{ useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
 import FirstHandleTooltip from "./FirstHandleTooltip";
 import SecondHandleTooltip from "./SecondHandleTooltip";
 import ThirdHandleTooltip from "./ThirdHandleTooltip";
 import FourthHandleTooltip from "./FourthHandleTooltip";
 import QuestionMark from "./images/question-mark.png";
-import { Link,useNavigate } from "react-router-dom";
+import { Alert } from "react-bootstrap";
 
 const TypeOfInformation = () => {
-
-    //Go to webpage
-    const navigate = useNavigate();
 
     //FirstHandleTooltip
     const [FirstModalShow, FirstSetModalShow] = React.useState(false);
@@ -23,39 +21,41 @@ const TypeOfInformation = () => {
     //FourthHandleTooltip
     const [FourthModalShow, FourthSetModalShow] = React.useState(false);
 
-    //Check the value of radio button
+
+
+    //Used for message error
+    const [error, setError] = useState("");
+    const [show, setShow] = useState(true);
+    const [message, setMessage] = useState("");
+
+    //Go to another page function
+    const navigate = useNavigate();
+
+
+
+    //Used to check the value of radio button
     const [selectedOption, setSelectedOption] = useState("");
 
 
-    const formRef = useRef(null);
-
+    //When change the option from radio button
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (selectedOption === "") {
-      alert("Please choose an option");
-      console.log("nao escolheu");
-    } else {
-      alert(`You chose ${selectedOption}`);
-      console.log("escolheu");
-    }
-  };
-
+  //Check if any button was choosen
   const handleButtonClick = () => {
     if (selectedOption === "") {
-      alert("Please choose an option");
-      console.log("No option selected");
+      // show the error message when field is empty
+      setError("Please select a response.");
+      setShow(true);
     } else {
-        navigate("/step5", { state: { selectedOption } });
+        navigate("/step5", { state: { selectedOption } }); //Go to page and pass data
     }
   };
 
     return (
 		<div className="container">
-			{/* Call popup function*/}
+
 			<FirstHandleTooltip
 				show={FirstModalShow}
 				onHide={() => FirstSetModalShow(false)}
@@ -88,59 +88,64 @@ const TypeOfInformation = () => {
 			<div className="border-decision-framework-pages">
 				<table>
                     <tbody>
-					<tr className="table-background">
-						<th>Type of information
-                            <img className="question-mark-pages"
-                                src={QuestionMark}
-                                alt="Qusestion Mark"
-                                border="0"
-                                onClick={() => SecondSetModalShow(true)}
-                            ></img>
-                        </th>
-					</tr>
-					<tr>
-						<td>
-							<p>What type of information will we have?</p>
-							<form className="radio-btn-section" ref={formRef} onSubmit={handleSubmit}>
-                                    <label>
-                                        <input
-                                            type="radio"
-                                            name="option"
-                                            value="explicit"
-                                            checked={selectedOption === "explicit"}
-                                            onChange={handleOptionChange}
-                                            className="radio-input low-rdb"
-                                        />
-                                        Explicit
-                                    </label>
-                                            <img className="question-mark-pages"
-                                                src={QuestionMark}
-                                                alt="Qusestion Mark"
-                                                border="0"
-                                                onClick={() => ThirdSetModalShow(true)}
-                                            ></img>
-                                    <label>
-                                        <input
-                                            type="radio"
-                                            name="option"
-                                            value="relative"
-                                            checked={selectedOption === "relative"}
-                                            onChange={handleOptionChange}
-                                            className="radio-input high-rdb"
-                                        />
-                                        Relative
+                        <tr className="table-background">
+                            <th>Type of information
+                                <img className="question-mark-pages"
+                                    src={QuestionMark}
+                                    alt="Qusestion Mark"
+                                    border="0"
+                                    onClick={() => SecondSetModalShow(true)}
+                                ></img>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td>
+                                {/* Show message em field is empty*/}
+                                {show && error && (
+                                <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+                                {error}
+                                </Alert>
+                                )}
+                                <p>What type of information will we have?</p>
+                                <form className="radio-btn-section">
+                                        <label>
+                                            <input
+                                                type="radio"
+                                                name="option"
+                                                value="explicit"
+                                                checked={selectedOption === "explicit"}
+                                                onChange={handleOptionChange}
+                                                className="radio-input low-rdb"
+                                            />
+                                            Explicit
+                                        </label>
+                                                <img className="question-mark-pages"
+                                                    src={QuestionMark}
+                                                    alt="Qusestion Mark"
+                                                    border="0"
+                                                    onClick={() => ThirdSetModalShow(true)}
+                                                ></img>
+                                        <label>
+                                            <input
+                                                type="radio"
+                                                name="option"
+                                                value="relative"
+                                                checked={selectedOption === "relative"}
+                                                onChange={handleOptionChange}
+                                                className="radio-input high-rdb"
+                                            />
+                                            Relative
 
-                                    </label>
-                                    <img className="question-mark-pages"
-                                                src={QuestionMark}
-                                                alt="Qusestion Mark"
-                                                border="0"
-                                                onClick={() => FourthSetModalShow(true)}
-                                            ></img>
-                                <button type="submit" style={{ display: "none" }} />
-				            </form>
-						</td>
-					</tr>
+                                        </label>
+                                        <img className="question-mark-pages"
+                                                    src={QuestionMark}
+                                                    alt="Qusestion Mark"
+                                                    border="0"
+                                                    onClick={() => FourthSetModalShow(true)}
+                                                ></img>
+                                </form>
+                            </td>
+                        </tr>
                     </tbody>
 				</table>
 			</div>
@@ -148,7 +153,6 @@ const TypeOfInformation = () => {
 			<div id="button-same-line">
 				<Link to="/Capacity" state= {{ capacitySelection: "teste" }}>	<button className="inner">BACK</button></Link>
 				<button className="inner" onClick={handleButtonClick}>NEXT</button>
-                {/* <Link to="/step5" state= {{  selectedOption   }} ><button className="inner" type="submit">NEXT</button>	</Link> */}
 			</div>
 		</div>
 	);
