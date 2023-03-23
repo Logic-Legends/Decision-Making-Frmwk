381f9085411c26726edef4eecd7548cddbce3b17
mhallaq <malhallaq@gmail.com>
2023-03-23 15:50:07 +0000

    changing cursor style on steps

--- client/src/components/ProgressBar/Step.js
+++ client/src/components/ProgressBar/Step.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Step(props) {
   return (
     <div className={"stepBlock" + (props.selected? " selected": "")}>
-    <div className={"circleWrapper" } onClick={()=>props.setStep(props.index+1)}>
+    <div className={"circleWrapper" } >
         <div className={"circle"}>{props.index+1}</div>
     </div>
     <span>{props.label}</span>
