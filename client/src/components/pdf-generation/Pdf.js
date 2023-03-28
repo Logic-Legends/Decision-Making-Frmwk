30c0a92347b51ee940adb9fb00223ba361c38f11
Ekrem <ekremteke.uk@gmail.com>
2023-03-28 10:19:17 +0100

    pdf background color

--- client/src/components/pdf-generation/Pdf.js
+++ client/src/components/pdf-generation/Pdf.js
@@ -3,7 +3,7 @@ import { Page, Text, View, StyleSheet } from "@react-pdf/renderer";
 
 const styles = StyleSheet.create({
     page: {
-        backgroundColor: "#E4E4E4",
+        backgroundColor: "white",
         flexDirection: "flex",
     },
     section: {
