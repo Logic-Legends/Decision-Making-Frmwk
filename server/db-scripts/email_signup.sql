1c9f8d03fa6d13cc75886ea7d218cf7d52c8145b
leilafaez <leila.faez@gmail.com>
2023-03-31 18:09:06 +0100

    change the Email text and add scripts

--- server/db-scripts/email_signup.sql
+++ server/db-scripts/email_signup.sql
@@ -1,5 +1,16 @@
-CREATE TABLE email_signup (
-    id bigserial primary key,
-    email varchar(255) NOT NULL,
-    date_added timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+-- Table: public.EMAIL_SIGNUP
+
+-- DROP TABLE IF EXISTS public."EMAIL_SIGNUP";
+
+CREATE TABLE IF NOT EXISTS public."EMAIL_SIGNUP"
+(
+    "ID" bigint NOT NULL DEFAULT nextval('"EMAIL_SIGNUP_ID_seq"'::regclass),
+    "EMAIL" character varying COLLATE pg_catalog."default",
+    "DATE_ADDED" time without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "EMAIL_SIGNUP_pkey" PRIMARY KEY ("ID")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."EMAIL_SIGNUP"
+    OWNER to postgres;
--- server/db-scripts/user_reviews.sql
+++ server/db-scripts/user_reviews.sql
@@ -1,6 +1,17 @@
-CREATE TABLE user_reviews (
-    review_id bigserial primary key,
-    review_text text NOT NULL,
-    user_name varchar(255),
-    date_added timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+-- Table: public.USER_REVIEWS
+
+-- DROP TABLE IF EXISTS public."USER_REVIEWS";
+
+CREATE TABLE IF NOT EXISTS public."USER_REVIEWS"
+(
+    "REVIEW_ID" bigint NOT NULL DEFAULT nextval('"USER_REVIEWS_REVIEW_ID_seq"'::regclass),
+    "REVIEW_TEXT" character varying(255) COLLATE pg_catalog."default",
+    "USER_NAME" character varying(255) COLLATE pg_catalog."default",
+    "DATE_ADDED" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "USER_REVIEWS_pkey" PRIMARY KEY ("REVIEW_ID")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."USER_REVIEWS"
+    OWNER to postgres;
